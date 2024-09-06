e645760c74a73f89693f150acc3e443c8d1eda5b
Dalbizurez <dalbizurez@correo.url.edu.gt>
2024-09-06 09:48:55 -0600

    Tabla hash
    Con manejo de colisiones por medio de una lista enlazada

---
+++ hashtable.ts
@@ -0,0 +1,79 @@
+class Linked_Node<T> {
+    private data: T;
+    private next!:Linked_Node<T>;
+
+    constructor(data:T){
+        this.data = data;
+    }
+
+    public setNext(next:Linked_Node<T>){
+        this.next = next
+    }
+    public getNext(){
+        return this.next;
+    }
+
+    public getData():T{
+        return this.data;
+    }
+
+    toString(){
+        return this.data;
+    }
+
+}
+
+export class Hashtable<T> {
+    private size: number;
+    protected data: Linked_Node<T>[];
+
+    constructor(size: number) {
+        this.data =  new Array(size);
+        this.size = size;
+    }
+
+    private hash(data: any): number {
+        return data % this.size;
+    }
+
+    public insert(data:T): void {
+        let index:number = this.hash(data);
+        if (!this.data[index]){
+            this.data[index] = new Linked_Node(data);
+        } else {
+            let node:Linked_Node<T> = this.data[index];
+            while (node.getNext()){
+                node = node.getNext();
+            }
+            node.setNext(new Linked_Node(data));
+        }
+    }
+
+    public search(data: number|string): T|string{
+        let index: number = this.hash(data);
+        let node = this.data[index];
+        if (typeof(data) == "string"){
+            data = parseInt(data.slice(1))
+        }
+        while (node){
+            if (node.getData() == data){
+                return node.getData()
+            }
+        }
+        return "No registrado"
+    }
+
+    toString() {
+        let txt:string = "";
+        for (let i = 0; i<this.data.length; i++){
+            txt += i+ ":";
+            let node = this.data[i];
+            while (node){
+                txt += " " + node.getData() + ",";
+                node = node.getNext();
+            }
+            txt += "\n";
+        }
+        return txt;
+    }
+}
